e0eec0a631383fd60a8ab50dd74f645c1e4b9f1e
btwwestflightexpress <148742167+btwwestflightexpress@users.noreply.github.com>
2023-11-18 11:36:06 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const crypto = require('crypto');
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.static(__dirname));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-const db = mysql.createPool({
+// Move the pool variable outside the connection callback
+const pool = mysql.createPool({
   host: process.env.DB_HOST || 'buuedht0tchqebrkwamg-mysql.services.clever-cloud.com',
   user: process.env.DB_USER || 'u1fbdrbtv5dvzads',
   password: process.env.DB_PASSWORD || '9lFiBkpEzHV9RwLkdKx',
@@ -30,10 +30,10 @@ const db = mysql.createPool({
 });
 
 // Handle connection errors
-db.promise().getConnection().then((connection) => {
+pool.promise().getConnection().then((connection) => {
   console.log('Connected to the database');
   connection.release(); // Release the initial connection
-  
+
   app.use(express.json());
   app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -43,8 +43,6 @@ db.promise().getConnection().then((connection) => {
 
     const sql = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
     const values = [username, email, password];
-
-    
 
     try {
       const [result] = await connection.query(sql, values);
@@ -65,7 +63,7 @@ db.promise().getConnection().then((connection) => {
     const query = "SELECT * FROM users WHERE username = ?";
 
     try {
-      const [results] = await db.promise().query(query, [username]);
+      const [results] = await pool.promise().query(query, [username]);
 
       if (results.length > 0) {
         const storedPassword = results[0].password;
@@ -99,13 +97,13 @@ db.promise().getConnection().then((connection) => {
     const quoteData = req.body; // Get quote details from the request
 
     // Insert the quote details into the database
-    const insertQuery = 'INSERT INTO freight_details (senders_name, Origin, origin_state, origin_zip_code, recievers_name, recievers_phone_number, recievers_email, destination, destination_state, destination_zip_code, package_name, package_type, package_weight, package_demension, package_number, tracking_code, package_password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    const insertQuery = 'INSERT INTO freight_details (senders_name, Origin, origin_state, origin_zip_code, recievers_name, recievers_phone_number, recievers_email, destination, destination_state, destination_zip_code, package_name, package_type, package_weight, package_demension, package_number, tracking_code, package_password, latitude, longitude) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
 
     // Generate a unique 10-character tracking code
     const trackingCode = generateTrackingCode();
 
     try {
-      const [result] = await db.promise().query(insertQuery, [quoteData.senders_name, quoteData.origin, quoteData.origin_state, quoteData.origin_zip_code, quoteData.recievers_name, quoteData.recievers_phone_number, quoteData.recievers_email, quoteData.destination, quoteData.destination_state, quoteData.destination_zip_code, quoteData.package_name, quoteData.package_type, quoteData.package_weight, quoteData.package_demension, quoteData.package_number, trackingCode, quoteData.package_password]);
+      const [result] = await pool.promise().query(insertQuery, [quoteData.senders_name, quoteData.origin, quoteData.origin_state, quoteData.origin_zip_code, quoteData.recievers_name, quoteData.recievers_phone_number, quoteData.recievers_email, quoteData.destination, quoteData.destination_state, quoteData.destination_zip_code, quoteData.package_name, quoteData.package_type, quoteData.package_weight, quoteData.package_demension, quoteData.package_number, trackingCode, quoteData.package_password, quoteData.latitude, quoteData.longitude]);
       console.log('Added new quote');
       res.json({ success: true, trackingCode });
     } catch (error) {
@@ -122,7 +120,7 @@ db.promise().getConnection().then((connection) => {
     const quoteQuery = 'SELECT * FROM freight_details WHERE tracking_code = ? AND package_password = ?';
 
     try {
-      const [quoteResults] = await db.promise().query(quoteQuery, [tracking_code, package_password]);
+      const [quoteResults] = await pool.promise().query(quoteQuery, [tracking_code, package_password]);
 
       if (quoteResults.length === 0) {
         return res.json({ success: false, message: 'Quote not found or password incorrect' });
@@ -136,7 +134,78 @@ db.promise().getConnection().then((connection) => {
     }
   });
 
-  // Additional routes and setup can go here
+  // Route to display an existing quote for editing
+  app.get('/quote/:trackingCode/edit', async (req, res) => {
+    const trackingCode = req.params.trackingCode;
+    try {
+      // Fetch quote details from the database based on trackingCode
+      const quote = await getQuoteByTrackingCode(trackingCode);
+      // Render a form for editing, with current details filled in
+      res.render('edit_quote', { quote });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+
+  // Route to handle quote update
+  app.post('/quote/:trackingCode/edit', async (req, res) => {
+    const trackingCode = req.params.trackingCode;
+    try {
+      // Update the quote details in the database based on trackingCode
+      await updateQuoteByTrackingCode(trackingCode, req.body);
+      // Redirect to a page displaying the updated quote
+      res.redirect('/dashboard.html'); // Change this to the actual route for your dashboard
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+
+  async function getQuoteByTrackingCode(trackingCode) {
+    const connection = await pool.promise().getConnection();
+    try {
+      const [rows] = await connection.query('SELECT * FROM freight_details WHERE tracking_code = ?', [trackingCode]);
+      return rows[0];
+    } finally {
+      connection.release();
+    }
+  }
+
+  async function updateQuoteByTrackingCode(trackingCode, { latitude, longitude }) {
+    const connection = await pool.promise().getConnection();
+    try {
+      await connection.query(
+        'UPDATE freight_details SET latitude = ?, longitude = ? WHERE tracking_code = ?',
+        [latitude, longitude, trackingCode]
+      );
+    } finally {
+      connection.release();
+    }
+  }
+
+  // Add this route to your server-side code
+  app.post('/retrieve-quote', async (req, res) => {
+    const trackingCode = req.body.trackingCode;
+
+    // Query the database to retrieve the quote details
+    const quoteQuery = 'SELECT * FROM freight_details WHERE tracking_code = ?';
+
+    try {
+      const [quoteResults] = await pool.promise().query(quoteQuery, [trackingCode]);
+
+      if (quoteResults.length === 0) {
+        return res.json({ success: false, message: 'Quote not found' });
+      }
+
+      const quote = quoteResults[0];
+      // Render a page with the retrieved quote details
+      res.render('edit_retrieved_quote', { quote });
+    } catch (error) {
+      console.error('Error retrieving quote:', error);
+      return res.json({ success: false, message: 'Failed to retrieve quote' });
+    }
+  });
 
   // Start the server
   app.listen(port, () => {
